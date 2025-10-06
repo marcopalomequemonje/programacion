b60e410f63a900ce5aa90060dba0456ff32df385
marco <marcopalomequemonje@gmail.com>
2025-10-06 13:12:53 +0200

    ejercicio 2 terminado

---
+++ practica2/src/ejercicio2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ejercicio2 {
+    static void main(String [] args) {
+        System.out.println("test");
+        //Crea un array de 5 numeros enteros
+        int[] a = {13,23,33,43,53};
+        for (int i=0; i<a.length; i++) {
+            System.out.print(i + "->"+a[i]+" " );
+        }
+    }
+}
+
