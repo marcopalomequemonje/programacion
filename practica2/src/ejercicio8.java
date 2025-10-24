973ae628dcdcb2ae3969891bf4c2a1bade595371
marco <marcopalomequemonje@gmail.com>
2025-10-24 08:44:23 +0200

    ejercicio8 terminado

---
+++ practica2/src/ejercicio8.java
@@ -0,0 +1,6 @@
+public class ejercicio8 {
+    public static void main(String[] args) {
+        System.out.println("Programa Java para leer la altura de N personas y calcular la altura media. Calcular cuántas personas tienen una altura superior a la media y cuántas tienen una altura inferior a la media. El valor de N se pide por teclado y debe ser entero positivo.");
+
+    }
+}
