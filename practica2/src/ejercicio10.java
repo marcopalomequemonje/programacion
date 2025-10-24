396a5bf6e03947452c71a2ca13135d3f041e593a
marco <marcopalomequemonje@gmail.com>
2025-10-24 11:35:43 +0200

    ejercicio10 empezado

---
+++ practica2/src/ejercicio10.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ejercicio10 {
+    public static void main(String[] args) {
+        System.out.println("Crea un programa que muestre por pantalla la nota de un estudiante, de entre una lista de estudiantes con sus respectivas notas. El nombre del estudiante lo introduce el usuario por teclado (usa la clase Scanner). Utiliza un array para los nombres de los alumnos y otro para las notas. ¿Serías capaz de hacerlo con un array BIdimensional?");
+        Scanner s = new Scanner(System.in);
+        //Crear Arrays
+        String[] aestudiantes={"Marco","Alvaro","Sergio","Raul","Cristian"};
+        double[] notas={10,9.7,4,2.5,8};
+    }
+}
