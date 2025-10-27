8093d18866179d14e5d500ad9affae6b8f6f48e8
marco <marcopalomequemonje@gmail.com>
2025-10-27 10:31:42 +0100

    ejercicio11 modificado

--- practica2/src/ejercicio11.java
+++ practica2/src/ejercicio11.java
@@ -1,5 +1,5 @@
 public class ejercicio11 {
-    public static void main() {
+    public static void main(String[] args) {
         System.out.println("Escribe un programa java que invierta el orden de los valores de un array. Por invertir el orden de los valores de un array, me refiero que el último pasa a ser el primero, el penúltimo el segundo y así sucesivamente. PRUEBA CON UN ARRAY DE TAMAÑO 6.");
     //crear array
         int [] a= {1,2,3,4,5,6};
