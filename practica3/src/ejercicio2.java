55d2363bdeb65b39dd14d203af1b3ac67ef56d65
marco <marcopalomequemonje@gmail.com>
2025-11-03 14:26:04 +0100

    ejercicio1 terminado

---
+++ practica3/src/ejercicio2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        System.out.println("Escribe un programa que solicite 20 números enteros. Estos números debemos de introducirlo en un array de 4 filas por 5 columnas. El programa mostrará las sumas parciales de filas y en las columnas el mayor número de la columna. La suma total debe aparecer en la esquina inferior derecha.\n");
+        Scanner sc = new Scanner(System.in);
+        //Pedimos por teclado los numeros
+        System.out.println("Introduce 20 numeros: ");
+        //Creamos el array
+        int[][] a = new int[4][5];
+        //Creamos un for para recorrer el array y almecenar los numeros escritos por teclado
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        //Bucle con for para calcular la suma de las filas
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("--------------------------------------------------------------");
+            int suma = 0;
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.printf("%10d|",a[i][j]);
+                suma += a[i][j];
+            }
+            System.out.println("∑: "+ suma);
+        }
+        System.out.println("--------------------------------------------------------------");
+        //Bucle para calcular el mayor de las columnas
+        for (int i = 0; i < a[0].length; i++) {
+            int mayor = a[0][i];
+            for (int j = 1; j < a.length; j++) {
+                if (a[j][i] > mayor) {
+                    mayor = a[j][i];
+                }
+            }
+            System.out.print("∑:" + mayor+ "   ");
+        }
+        System.out.println();
+        System.out.println("--------------------------------------------------------------");
+    }
+}
