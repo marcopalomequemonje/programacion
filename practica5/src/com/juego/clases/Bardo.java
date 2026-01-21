592a1f7cd46d81ed9fe23480c732eff9ec8c8a4a
marco <marcopalomequemonje@gmail.com>
2026-01-21 14:23:57 +0100

    rellenar datos de la funcion estadisticas de bardo

--- practica5/src/com/juego/clases/Bardo.java
+++ practica5/src/com/juego/clases/Bardo.java
@@ -3,25 +3,18 @@ package com.juego.clases;
 import com.juego.modelo.Estadisticas;
 //ATRIBUTOS
 public class Bardo implements Clase {
-    private Estadisticas estadisticas;
+
 //CONTRUCTOR
-    public Bardo(Estadisticas estadisticas) {
-        this.estadisticas = estadisticas;
-    }
-//METODOS GET Y SET
+    public Bardo() {
 
-    public Estadisticas getEstadisticas() {
-        return estadisticas;
     }
 
-    public void setEstadisticas(Estadisticas estadisticas) {
-        this.estadisticas = estadisticas;
-    }
     //USAMOS OVERRIDE PARA SOBREESCRIBIR
     @Override
     //FUNCIONES
     public void bonificadorEstadisticas(Estadisticas estadisticas) {
-
+        estadisticas.setInteligencia(estadisticas.getInteligencia()+3);
+        estadisticas.setVida(estadisticas.getVida()+90);
     }
 }
 
