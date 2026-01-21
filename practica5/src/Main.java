aa6ff5c0233f17a80a19a1fe44774d984442ff97
marco <marcopalomequemonje@gmail.com>
2026-01-21 14:25:17 +0100

    meter datos a fuego en main sobre personaje para hacer pruebas

--- practica5/src/Main.java
+++ practica5/src/Main.java
@@ -1,13 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import com.juego.clases.Bardo;
+import com.juego.modelo.Personaje;
+import com.juego.razas.Humano;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main{
+    public static void main(String[] args) {
+        Personaje personaje1=new Personaje("Marco",new Humano(),new Bardo());
+        System.out.println(personaje1.getEstadisticas().comprobarInteligencia(5));
     }
 }
